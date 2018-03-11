802fa557b8ce66c6ef00a99b60db947866975446
Kishor Bikram Oli <geek.kishor@gmail.com>
2018-03-11 21:24:08 +0545

    Kotlin-Ranges

--- app/src/main/java/com/example/kotlin_helloworld/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_helloworld/MainActivity.kt
@@ -9,104 +9,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         /**
-         * Variables
+         * Ranges
          */
 
-        var name: String;
-        var name1: String = "Kishor";  //Initialize
+        var range = 1..10  //this means 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
-        println("My name  is " + name1);
+        for (i in range) {
+            println(i)
+        }
 
-        /**
-         * Constants
-         */
-
-        val constVariable: Int = 2
-        //constVariable = 7 //Gives error, we cannot reassign new value to constant variable
-
-
-        var name2 = "Abcdef"
-        val constVariable2 = 2
+        //Output: 1 2 3 4 5 6 7 8 9 10
 
 
         /**
-         * Arrays
+         * downTo
          */
-        var arr = arrayOf("Apple", "Mango", 2, 4.6) //array of different data types
 
-        var arr1: Array<String> = arrayOf("Apple", "Mango", "Orange")  //array of string data type
+        var a = 10 downTo 1  //this means 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
-        var arr2: Array<Int> = arrayOf(1, 2, 3, 4, 5) //array of integer data type
+        for (i in a) {
+            println(i)
+        }
+        //Output: 10 9 8 7 6 5 4 3 2 1
 
 
         /**
-         * Major Data Types
+         * downTo with step
          */
 
-        val a: Int = 5
-        val b: Float = 5.00f
-        val c: Double = 5.00
-        val d: Long = 5000000000
-        val e: Short = 50
-        val f: Byte = 1
+        var b = 10 downTo 1 step 2 //add range with interval 2. this means 10, 8, 6, 4, 2, 1
 
-        println("Integer " + a);
-        println("Float " + b);
-        println("Double " + c);
-        println("Long " + d);
-        println("Short " + d);
-        println("Byte " + f);
+        for (i in b) {
+            println(i)
+        }
+        //Output: 10 8 6 4 2 1
 
 
         /**
-         * Characters
+         * downTo with reversed
          */
-        val address: Char
-        address = 'K'
-        println(address)
-        println("$address")  //we can also print like this
 
+        var c = 10 downTo 1  //this means 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
-        /**
-         *Boolean
-         */
-        val isPresent: Boolean
-        isPresent = false
-        println(isPresent)
-        println("$isPresent")  //we can also print like this
+        for (i in c.reversed()) {
+            println(i)
+        }
+        //Output: 1 2 3 4 5 6 7 8 9 10
 
-        /**
-         *Strings
-         */
-        val myName: String
-        myName = "My name is Kishor"
-        println(myName)
-        println("$myName")  //we can also print like this
-
-        /**
-         * Collections
-         */
-        val fruits = listOf("Apple", "Banana", "Grapes")
-
-        val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
-
-        val info = mapOf(
-                Pair("name", "kishor"),
-                Pair("age", "24"))
-
-
-        println("First fruit = " + fruits.first())  //Outputs Apple
-        println("Last fruit = " + fruits.last())   //Outputs Grapes
-
-        println("List of even numbers = " +
-                numbers.filter { it % 2 == 0 })   // Outputs [2, 4, 6, 8]
-
-        println("List of odd numbers =  " +
-                numbers.filter { it % 2 == 1 }) //Outputs [1, 3, 5, 7]
-
-        println("Name = " + info["name"])  //Outputs kishor
-        println("Age = " + info["age"]) //Outputs 24
     }
 }
